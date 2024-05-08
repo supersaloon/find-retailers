8e1ae3eb008142d281b61a13d3c05e04fcbe7768
Gaebal-Dragon <geabal.dragon@gmail.com>
2024-05-08 10:21:51 +0900

    chanage thread_id2

--- utils.py
+++ utils.py
@@ -19,7 +19,7 @@ def show_in_chain(inputs):
     return inputs
 
 
-def generate_random_string(length=10):
+def generate_random_string(length=20):
     # string.ascii_letters는 대소문자 알파벳을 모두 포함하며, string.digits는 숫자 0-9를 포함한다.
     characters = string.ascii_letters + string.digits
     # random.choices 함수를 사용하여 주어진 문자열에서 임의의 문자를 선택하고, 이를 지정된 길이만큼 반복한다.
